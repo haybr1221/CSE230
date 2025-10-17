b98a395d07279d2122415d3896345efdaf076cbf
gratiam <104708129+gratiam@users.noreply.github.com>
2025-10-17 17:32:34 -0600

    implemented lander reset

--- 230.05.Ponder/lander.cpp
+++ 230.05.Ponder/lander.cpp
@@ -16,7 +16,22 @@
   ***************************************************************/
 void Lander :: reset(const Position & posUpperRight)
 {
-   status = DEAD;
+
+    //Set the angle of the lander to straight up.
+    angle.setDegrees(0.0);
+    //Set the status of the lander to PLAYING.
+    status = PLAYING;
+    //Fill the fuel with 5000.0 units.
+    fuel = 5000.0;
+    //Set the horizontal velocity to a random value between - 4m / s and -10m / s.
+    velocity.setDX(random(-10, -4));
+    //Set the vertical velocity to a random value between - 2m / s and 2m / s.
+    velocity.setDY(random(-2, 2));
+    //Set the horizontal position to 1 pixel from the right of the screen.
+    pos.setX(posUpperRight.getX() - 1);
+    //Set the vertical position that is between 75 % and 95 % of the height of the screen.You will need to use the random() function to do that.
+    double screenHeight = posUpperRight.getY();
+    pos.setY(random((screenHeight * 0.75), (screenHeight * 0.95)));
 }
 
 /***************************************************************
