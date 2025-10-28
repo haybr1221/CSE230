f545a7be4e2af168e7b93749802e2a9f1cf98b38
Hayley <bra22025@byui.edu>
2025-10-28 14:09:52 -0600

    problem set

--- 230.07.CppPointer/wallet.h
+++ 230.07.CppPointer/wallet.h
@@ -18,7 +18,53 @@ class TestWallet;
   ************************************/
 class Wallet
 {
-   friend TestWallet;
+      friend TestWallet;
+private:
+   double accountSam;
+   double accountSue;
+   double* pCurrentAccount;
 
+public:
+   // Constructors
+   Wallet() : accountSam{0.0}, accountSue{0.0}, pCurrentAccount{&accountSam} {};
+   
+   // Setters
+   void updateSam(double amount)
+   {
+      accountSam += amount;
+   };
+   void updateSue(double amount)
+   {
+      accountSue += amount;
+   };
+   void updateCurrent(double amount)
+   {
+      *pCurrentAccount += amount;
+   };
+   
+   void chooseSam()
+   {
+      pCurrentAccount = &accountSam;
+   };
+   void chooseSue()
+   {
+      pCurrentAccount = &accountSue;
+   };
+   void chooseCurrent()
+   {
+      if (accountSam > accountSue)
+      {
+         chooseSam();
+      }
+      else
+      {
+         chooseSue();
+      }
+   };
+   
+   // Getters
+   double getSam()const       { return accountSam; };
+   double getSue()const       { return accountSue; };
+   double getCurrent()const   { return *pCurrentAccount; };
 };
 
