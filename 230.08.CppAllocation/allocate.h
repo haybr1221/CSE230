81cd35aa90d2d250bf8e8802e0352da81b2fe8ce
Hayley <bra22025@byui.edu>
2025-11-04 12:26:59 -0700

    w8 prob set files

---
+++ 230.08.CppAllocation/allocate.h
@@ -0,0 +1,32 @@
+/*************************************************************
+ * 1. Name:
+ *      -your name-
+ * 2. Assignment Name:
+ *      Practice 08: Allocation
+ * 3. Assignment Description:
+ *      Allocate an array and then use it
+ * 4. What was the hardest part? Be as specific as possible.
+ *      -a paragraph or two about how the assignment went for you-
+ * 5. How long did it take for you to complete the assignment?
+ *      -total time in hours: reading the assignment, submitting, etc.
+ **************************************************************/
+
+ /****************************
+  * ALLOCATE ONE FLOAT
+  ****************************/
+
+
+
+/****************************
+ * ALLOCATE ARRAY of DOUBLEs
+ ****************************/
+
+
+/****************************
+ * DELETE ONE FLOAT
+ ****************************/
+
+
+/****************************
+ * DELETE ARRAY of DOUBLEs
+ ****************************/
