2a72ec93fac7453fb416d4a308c1e79e8a1e74e8
Hayley <bra22025@byui.edu>
2025-11-04 18:29:15 -0700

    w8 allocation

--- 230.08.CppAllocation/allocate.h
+++ 230.08.CppAllocation/allocate.h
@@ -1,32 +1,58 @@
 /*************************************************************
  * 1. Name:
- *      -your name-
+ *      Hayley Branchflower & Graham McClure
  * 2. Assignment Name:
  *      Practice 08: Allocation
  * 3. Assignment Description:
  *      Allocate an array and then use it
  * 4. What was the hardest part? Be as specific as possible.
- *      -a paragraph or two about how the assignment went for you-
+ *      The hardest part of the assignement was figuring out the inline functions and handling arrays.
  * 5. How long did it take for you to complete the assignment?
- *      -total time in hours: reading the assignment, submitting, etc.
+ *      1 hour
  **************************************************************/
 
  /****************************
   * ALLOCATE ONE FLOAT
   ****************************/
 
-
+inline float* allocateOneFloat(float newFloat)
+{
+   float* ptr = new float(newFloat);
+   return ptr;
+};
 
 /****************************
  * ALLOCATE ARRAY of DOUBLEs
  ****************************/
 
+inline double* allocateArrayDouble(int size)
+{
+   if (size <= 0)
+      return nullptr;
+   return new double[size];
+};
 
 /****************************
  * DELETE ONE FLOAT
  ****************************/
-
+void deleteOneFloat(float*& ptr)
+{
+   if (ptr != nullptr)
+   {
+      delete ptr;
+      ptr = nullptr;
+   }
+};
 
 /****************************
  * DELETE ARRAY of DOUBLEs
  ****************************/
+
+void deleteArrayDouble(double*& ptr)
+{
+   if (ptr != nullptr)
+   {
+      delete[] ptr;
+      ptr = nullptr;
+   }
+};
